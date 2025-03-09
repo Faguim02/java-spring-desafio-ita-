5d78503dd6d6d13f09d7f0b55c6b28267a1ccb06
Faguim02 <fagnermuniz12@gmail.com>
2025-03-09 18:07:44 -0300

    criando projeto spring

---
+++ src/main/java/com/example/itau/JavaSpringDesafioItauApplication.java
@@ -0,0 +1,13 @@
+package com.example.itau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDesafioItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDesafioItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itau/JavaSpringDesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
