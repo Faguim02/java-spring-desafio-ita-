fb11e9281f54b6e2f313e48f8be6d1ca4babc280
Faguim02 <fagnermuniz12@gmail.com>
2025-03-10 09:49:45 -0300

    adicionando as regras no metodo receiveTransaction

--- src/main/java/com/example/itau/controller/TransacaoController.java
+++ src/main/java/com/example/itau/controller/TransacaoController.java
@@ -5,9 +5,12 @@ import com.example.itau.repository.TransacaoRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
 
 @RestController
 public class TransacaoController {
@@ -18,8 +21,22 @@ public class TransacaoController {
     @PostMapping("/transacao")
     ResponseEntity<Void> receiveTransaction(@RequestBody TransacaoModel transacaoReq) {
 
-        this.transacaoRespository.save(transacaoReq);
+        try {
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+            if (transacaoReq.getValor() == null || transacaoReq.getValor() < 0 || transacaoReq.getDataHora() == null) {
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+            if (OffsetDateTime.now().isBefore(transacaoReq.getDataHora())) {
+                return ResponseEntity.unprocessableEntity().build();
+            }
+
+            this.transacaoRespository.save(transacaoReq);
+
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+
+        } catch (HttpMessageNotReadableException ex) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
