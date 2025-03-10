950f1f962ef639d6f6dd7f009a170a9e938f7382
Faguim02 <fagnermuniz12@gmail.com>
2025-03-10 09:12:31 -0300

    criando o metodo 'receiveTransaction' que vai receber a transação e armazenar

--- src/main/java/com/example/itau/controller/TransacaoController.java
+++ src/main/java/com/example/itau/controller/TransacaoController.java
@@ -1,7 +1,12 @@
 package com.example.itau.controller;
 
+import com.example.itau.model.TransacaoModel;
 import com.example.itau.repository.TransacaoRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,11 @@ public class TransacaoController {
     @Autowired
     TransacaoRespository transacaoRespository;
 
+    @PostMapping("/transacao")
+    ResponseEntity<Void> receiveTransaction(@RequestBody TransacaoModel transacaoReq) {
+
+        this.transacaoRespository.save(transacaoReq);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
