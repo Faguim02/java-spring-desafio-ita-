ed3f3438c0619f0f2234740e376437730a71ae76
Faguim02 <fagnermuniz12@gmail.com>
2025-03-12 11:43:28 -0300

    adicionando os logs para a minha aplicação

--- src/main/java/com/example/itau/controller/TransacaoController.java
+++ src/main/java/com/example/itau/controller/TransacaoController.java
@@ -13,27 +13,39 @@ import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class TransacaoController {
 
     @Autowired
     TransacaoRespository transacaoRespository;
+    Logger logger = Logger.getLogger(TransacaoController.class.getName());
 
     @PostMapping("/transacao")
     ResponseEntity<Void> receiveTransaction(@RequestBody TransacaoModel transacaoReq) {
 
         try {
 
+            logger.log(Level.INFO, "Verificando os dados do usuario...");
+
             if (transacaoReq.getValor() == null || transacaoReq.getValor() < 0 || transacaoReq.getDataHora() == null) {
+                logger.log(Level.WARNING, "o valor da transação ou dataHora está vazia");
                 return ResponseEntity.unprocessableEntity().build();
             }
 
             if (OffsetDateTime.now().isBefore(transacaoReq.getDataHora())) {
+                logger.log(Level.WARNING, "a data da transação está invalida, data futura");
                 return ResponseEntity.unprocessableEntity().build();
             }
 
+            logger.log(Level.INFO, "Os dados estão corretos!");
+            logger.log(Level.INFO, "Salvando informações...");
+
             this.transacaoRespository.save(transacaoReq);
+
+            logger.log(Level.INFO, "Salvo com sucesso!");
 
             return ResponseEntity.status(HttpStatus.CREATED).build();
 
@@ -44,22 +56,33 @@ public class TransacaoController {
 
     @DeleteMapping("/transacao")
     ResponseEntity<Void> clearTransactions() {
+        logger.log(Level.INFO, "Deletando transações...");
+
         this.transacaoRespository.clear();
+
+        logger.log(Level.INFO, "Deletado com sucesso!");
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/estatistica")
     ResponseEntity<EstatisticaResDto> calculateStatistic() {
+        logger.log(Level.INFO, "Buscando transações...");
+
         List<TransacaoModel> transacaoModels = this.transacaoRespository.findAllTransactions();
 
         OffsetDateTime dateActual = OffsetDateTime.now();
 
         Duration duration = Duration.between(transacaoModels.getLast().getDataHora(), dateActual);
 
+        logger.log(Level.INFO, "Verificando data das transações...");
+
         if (duration.getSeconds() > 60) {
+            logger.log(Level.WARNING, "a data das transações foram a mais de 60 segundos atrás");
             return ResponseEntity.status(HttpStatus.OK).body(new EstatisticaResDto(0l,0.0,0.0,0.0,0.0));
         }
+
+        logger.log(Level.INFO, "Calculando...");
 
         DoubleSummaryStatistics statistics = transacaoModels.stream()
                 .mapToDouble(TransacaoModel::getValor)
@@ -73,12 +96,14 @@ public class TransacaoController {
                 statistics.getMax()
         );
 
+        logger.log(Level.INFO, "OK!");
+
         return ResponseEntity.status(HttpStatus.OK).body(estatisticaResDto);
 
     }
 
     @GetMapping("generate")
-    String act() {
+    String generateDateActual() {
         return OffsetDateTime.now().toString();
     }
 }
