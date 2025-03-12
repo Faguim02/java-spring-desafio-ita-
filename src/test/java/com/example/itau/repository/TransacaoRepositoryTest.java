7fb6c8a25173a5089aa773cd34fc54011235afe2
Faguim02 <fagnermuniz12@gmail.com>
2025-03-11 21:28:04 -0300

    criando classe de tests para a camada de repository

---
+++ src/test/java/com/example/itau/repository/TransacaoRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.itau.repository;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("transactio repository test")
+public class TransacaoRepositoryTest {
+
+    @InjectMocks
+    private TransacaoRespository transacaoRespository;
+
+    class Save {
+
+    }
+
+    class Clear {
+
+    }
+
+    class FindAllTransaction {
+
+    }
+
+}
