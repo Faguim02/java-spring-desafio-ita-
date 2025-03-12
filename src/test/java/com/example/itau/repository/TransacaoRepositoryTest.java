c3eae4e5efcaba57c37a52eaa682b957700d3dd8
Faguim02 <fagnermuniz12@gmail.com>
2025-03-11 22:13:36 -0300

    criando metodo que testa se o metodo 'clear()' está deletando todas as transações corretamentes

--- src/test/java/com/example/itau/repository/TransacaoRepositoryTest.java
+++ src/test/java/com/example/itau/repository/TransacaoRepositoryTest.java
@@ -3,7 +3,6 @@ package com.example.itau.repository;
 import com.example.itau.model.TransacaoModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("transactio repository test")
@@ -31,6 +31,16 @@ public class TransacaoRepositoryTest {
 
         Assertions.assertEquals(transacaoModels.getFirst(), transacaoModel);
 
+    }
+
+    @Test
+    void shouldClearAllTransaction() {
+
+        this.shoudSaveTransaction();
+
+        this.transacaoRespository.clear();
+
+        Assertions.assertEquals(transacaoRespository.findAllTransactions(), new CopyOnWriteArrayList<>());
     }
 
 }
