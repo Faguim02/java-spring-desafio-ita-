880d1c7cddac1757bee2720b9acb95c817ae7d21
Faguim02 <fagnermuniz12@gmail.com>
2025-03-11 21:52:42 -0300

    criando metodo para testar se o metodo 'save' está salvando as transações corretamente

--- src/test/java/com/example/itau/repository/TransacaoRepositoryTest.java
+++ src/test/java/com/example/itau/repository/TransacaoRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.example.itau.repository;
 
+import com.example.itau.model.TransacaoModel;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("transactio repository test")
@@ -15,6 +21,20 @@ public class TransacaoRepositoryTest {
 
     @Nested
     class Save {
+
+        @Test
+        void shoudSaveTransaction() {
+            TransacaoModel transacaoModel = new TransacaoModel();
+            transacaoModel.setValor(10.5);
+            transacaoModel.setDataHora(OffsetDateTime.now());
+
+            transacaoRespository.save(transacaoModel);
+
+            List<TransacaoModel> transacaoModels = transacaoRespository.findAllTransactions();
+
+            Assertions.assertEquals(transacaoModels.getFirst(), transacaoModel);
+
+        }
 
     }
 
